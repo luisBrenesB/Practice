a8ec91df57aa5849991eb4fadfa1e5a2eba04b48
Luis <luis.brenes@alumnos.uneatlantico.es>
2025-01-16 22:36:25 +0100

    Estructura inicial del programa

---
+++ CapturaElDragon.java
@@ -0,0 +1,6 @@
+public class CapturaElDragon {
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenido al juego 'Captura al Dragón'!");
+        System.out.println("El dragón está escondido en una de las cuevas. ¡Buena suerte!");
+    }
+}
