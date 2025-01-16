a1abf2f2e767b5225cc84de8e827419b6c5a1901
Luis <luis.brenes@alumnos.uneatlantico.es>
2025-01-16 22:39:22 +0100

    Constantes y variables principales para el juego.

--- CapturaElDragon.java
+++ CapturaElDragon.java
@@ -1,6 +1,20 @@
 public class CapturaElDragon {
     public static void main(String[] args) {
+        
+        final int NUMERO_DE_TURNOS = 10;
+        final int NUMERO_CUEVAS = 100; 
+
+
+        boolean capturado = false;
+        int posicionDragon = generarPosicionInicial(NUMERO_CUEVAS);
+        int turno = 0; 
+
         System.out.println("¡Bienvenido al juego 'Captura al Dragón'!");
-        System.out.println("El dragón está escondido en una de las cuevas. ¡Buena suerte!");
+        System.out.println("El dragón está escondido en una de las " + NUMERO_CUEVAS + " cuevas. ¡Buena suerte!");
+    }
+
+
+    static int generarPosicionInicial(int maxCuevas) {
+        return (int) (Math.random() * maxCuevas) + 1; 
     }
 }
